903c7a2ba4b1dfe3e4c13b573aaf650e506f08e9
Fate <d3veloperfate@gmail.com>
2025-10-13 13:13:12 +0200

    feat(ratatui): adding crate + min interface layout

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+use color_eyre::Result;
+use ratatui::crossterm::event::{self, Event, KeyCode};
+use ratatui::{DefaultTerminal, Frame};
+
+fn main() -> Result<()> {
+    color_eyre::install()?;
+    let terminal = ratatui::init();
+    let result = run(terminal);
+    ratatui::restore();
+    result
+}
+
+fn run(mut terminal: DefaultTerminal) -> Result<()> {
+    loop {
+        terminal.draw(render)?;
+
+        if let Event::Key(key) = event::read()? {
+            match key.code {
+                KeyCode::Char('q') | KeyCode::Esc => break,
+                _ => {}
+            }
+        }
+    }
+
+    Ok(())
+}
+
+fn render(frame: &mut Frame) {
+    frame.render_widget("todo-tui", frame.area());
 }
