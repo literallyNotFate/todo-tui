7453f0826a8ad27c52ba8cbb73a5eab2198c79c3
Fate <d3veloperfate@gmail.com>
2026-01-26 19:57:52 +0100

    core: created_at field in todo model w/time_ago() using chrono

--- src/models/todo.rs
+++ src/models/todo.rs
@@ -1,4 +1,5 @@
 use super::Priority;
+use chrono::{DateTime, TimeDelta, Utc};
 use serde::{Deserialize, Serialize};
 use std::hash::Hash;
 use uuid::Uuid;
@@ -10,6 +11,7 @@ pub struct Todo {
     pub description: String,
     pub completed: bool,
     pub priority: Priority,
+    pub created_at: DateTime<Utc>,
 }
 
 impl Todo {
@@ -24,11 +26,44 @@ impl Todo {
             description: description.into(),
             completed: false,
             priority: priority.unwrap_or_default(),
+            created_at: Utc::now(),
         }
     }
 
     pub fn toggle_completed(&mut self) {
         self.completed = !self.completed;
+    }
+
+    pub fn time_ago(&self) -> String {
+        let now: DateTime<Utc> = Utc::now();
+        let time_passed: TimeDelta = now.signed_duration_since(self.created_at);
+        let days: i64 = time_passed.num_days();
+
+        let format_unit = |count: i64, unit: &str| -> String {
+            if count == 1 {
+                format!("{} {} ago", count, unit)
+            } else {
+                format!("{} {}s ago", count, unit)
+            }
+        };
+
+        if days >= 365 {
+            format_unit(days / 365, "year")
+        } else if days >= 30 {
+            format_unit(days / 30, "month")
+        } else if time_passed.num_weeks() > 0 {
+            format_unit(time_passed.num_weeks(), "week")
+        } else if days > 0 {
+            format_unit(days, "day")
+        } else if time_passed.num_hours() > 0 {
+            format_unit(time_passed.num_hours(), "hour")
+        } else if time_passed.num_minutes() > 0 {
+            format_unit(time_passed.num_minutes(), "minute")
+        } else if time_passed.num_seconds() > 0 {
+            format_unit(time_passed.num_seconds(), "second")
+        } else {
+            "just now".to_string()
+        }
     }
 }
 
@@ -36,6 +71,7 @@ impl Todo {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use chrono::{Days, Months};
 
     #[test]
     fn should_create_todo_item() {
@@ -75,5 +111,23 @@ mod tests {
 
         todo.toggle_completed();
         assert!(!todo.completed);
+    }
+
+    #[test]
+    fn should_return_created_at_string() {
+        let mut todo: Todo = Todo::new("Test", "Test", None);
+        assert_eq!(todo.time_ago(), "just now".to_string());
+
+        todo.created_at = Utc::now().checked_sub_days(Days::new(2)).unwrap();
+        assert_eq!(todo.time_ago(), "2 days ago".to_string());
+
+        todo.created_at = Utc::now().checked_sub_days(Days::new(7)).unwrap();
+        assert_eq!(todo.time_ago(), "1 week ago".to_string());
+
+        todo.created_at = Utc::now().checked_sub_months(Months::new(3)).unwrap();
+        assert_eq!(todo.time_ago(), "3 months ago");
+
+        todo.created_at = Utc::now().checked_sub_days(Days::new(365)).unwrap();
+        assert_eq!(todo.time_ago(), "1 year ago");
     }
 }
